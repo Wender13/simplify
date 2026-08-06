b8e7b67225669a3b6a5e06c3f7716d16ca47e352
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-06 13:44:43 -0300

    fix/change sql file to match updated variable's name

--- src-tauri/migrations/002_category.sql
+++ src-tauri/migrations/002_category.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS category (
     id_category INTEGER PRIMARY KEY AUTOINCREMENT,
     id_user   INTEGER NOT NULL,
     name         TEXT NOT NULL,
-    type         TEXT NOT NULL CHECK (tipo IN ('INCOME', 'EXPENSE')),
+    type         TEXT NOT NULL CHECK (type IN ('INCOME', 'EXPENSE')),
     color          TEXT,
     icon        TEXT,
     FOREIGN KEY (id_user) REFERENCES user(id_user)
