99a307f196b0cebac12fd7b6fff0fcdf8e95e190
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-09 10:53:53 -0300

    fix/updates function to use sqlx migration function

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -18,33 +18,7 @@ pub fn pool() -> SqlitePool {
 }
 
 pub async fn run_migrations(pool: &SqlitePool) -> Result<(), sqlx::Error> {
-    sqlx::query(include_str!("../migrations/001_user.sql"))
-        .execute(pool)
-        .await?;
-
-    sqlx::query(include_str!("../migrations/002_category.sql"))
-        .execute(pool)
-        .await?;
-
-    sqlx::query(include_str!("../migrations/003_account.sql"))
-        .execute(pool)
-        .await?;
-
-    sqlx::query(include_str!("../migrations/004_transaction.sql"))
-        .execute(pool)
-        .await?;
-
-    sqlx::query(include_str!("../migrations/005_financial_target.sql"))
-        .execute(pool)
-        .await?;
-
-    sqlx::query(include_str!("../migrations/006_indexes.sql"))
-        .execute(pool)
-        .await?;
-
-    sqlx::query(include_str!("../migrations/007_trigger.sql"))
-        .execute(pool)
-        .await?;
+    sqlx::migrate!("./migrations").run(pool).await.expect("Failed to run sql migration files!");
 
     Ok(())
 }
