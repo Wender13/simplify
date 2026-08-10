82737d545ef74d5badf08a4d503eba0b1248b93d
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-10 13:58:24 -0300

    Reapply "style/nothing importante, just added a comma"
    This reverts commit 2d8ac88aad23d9af341e65d6db46cdb7e21e9168.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    simplify_lib::run()
+    simplify_lib::run();
 }
--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -1,11 +1,11 @@
-pub mod user;
-pub mod account;
-pub mod category;
-pub mod financial_target;
-pub mod transaction;
+// pub mod user;
+// pub mod account;
+// pub mod category;
+// pub mod financial_target;
+// pub mod transaction;
 
-pub use user::User;
-pub use account::Account;
-pub use category::Category;
-pub use financial_target::FinancialTarget;
-pub use transaction::Transaction;
+// pub use user::User;
+// pub use account::Account;
+// pub use category::Category;
+// pub use financial_target::FinancialTarget;
+// pub use transaction::Transaction;
