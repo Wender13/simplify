14edf16199934682fe475598fb9ef15565d59257
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-09 21:18:43 -0300

    feat/add delete and create user functions

--- src-tauri/src/controllers/user_controller.rs
+++ src-tauri/src/controllers/user_controller.rs
@@ -1,6 +1,52 @@
-use sqlx::SqlitePool;
-use User;
+use crate::dto::command_result::CommandResult;
+use crate::repository::user_repository::{self, CreateUserResult, DeleteUserResult};
 
-pub fn create_user(name: String, email: String) -> Result<User, String> {
-  
+pub async fn create_user(
+    name: String,
+    email: String,
+    password: String,
+) -> Result<CommandResult, String> {
+    let name = name;
+    let email: String = email.to_lowercase().trim().into();
+    let password = password.trim();
+
+    if name.is_empty() || email.is_empty() || password.is_empty() {
+        return Ok(CommandResult {
+            success: false,
+            message_key: "user.emptyUserData".into(),
+        });
+    }
+
+    match user_repository::create_user(name, email.into(), password.into()).await? {
+        CreateUserResult::Created => Ok(CommandResult {
+            success: true,
+            message_key: "user.userCreated".into(),
+        }),
+        CreateUserResult::EmailTaken => Ok(CommandResult {
+            success: false,
+            message_key: "user.emailTaken".into(),
+        }),
+    }
+}
+
+pub async fn delete_user(email: String) -> Result<CommandResult, String> {
+    let email = email.to_lowercase();
+
+    if email.is_empty() {
+        return Ok(CommandResult {
+            success: false,
+            message_key: "user.emptyUserData".into(),
+        });
+    }
+
+    match user_repository::delete_user(email).await? {
+        DeleteUserResult::Deleted => Ok(CommandResult {
+            success: true,
+            message_key: "user.userDeleted".into(),
+        }),
+        DeleteUserResult::UserNotFound => Ok(CommandResult {
+            success: false,
+            message_key: "user.userNotFound".into(),
+        }),
+    }
 }
