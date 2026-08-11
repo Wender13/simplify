9060ad25722b34c21e4fec7d75f1992b011753ae
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-11 14:43:58 -0300

    feat/adds user repository get, update and delete methods

--- src-tauri/src/repository/user_repository.rs
+++ src-tauri/src/repository/user_repository.rs
@@ -1,5 +1,5 @@
 use crate::db;
-
+use crate::models::User;
 pub enum CreateUserResult {
     Created,
     EmailTaken,
@@ -10,19 +10,9 @@ pub enum DeleteUserResult {
     UserNotFound,
 }
 
-pub async fn delete_user(email: String) -> Result<DeleteUserResult, String> {
-    let pool = db::pool();
-    let result = sqlx::query("DELETE FROM user WHERE email = ?")
-        .bind(email)
-        .execute(&pool)
-        .await
-        .map_err(|e| e.to_string())?;
-
-    if result.rows_affected() == 0 {
-        Ok(DeleteUserResult::UserNotFound)
-    } else {
-        Ok(DeleteUserResult::Deleted)
-    }
+pub enum UpdateUserResult {
+    Updated,
+    UserNotFound,
 }
 
 pub async fn create_user(
@@ -44,5 +34,98 @@ pub async fn create_user(
             Ok(CreateUserResult::EmailTaken)
         }
         Err(e) => Err(e.to_string()),
+    }
+}
+
+pub async fn get_users() -> Result<Vec<User>, String> {
+    let pool = db::pool();
+    sqlx::query_as::<_, User>("SELECT * FROM user")
+        .fetch_all(&pool)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+pub async fn get_user(email: String) -> Result<Option<User>, String> {
+    let pool = db::pool();
+    sqlx::query_as::<_, User>("SELECT * FROM user WHERE email = ?")
+        .bind(email)
+        .fetch_optional(&pool)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+pub async fn update_user_complete(
+    name: String,
+    old_email: String,
+    new_email: String,
+    password: String,
+) -> Result<UpdateUserResult, String> {
+    let pool = db::pool();
+
+    let result = sqlx::query("UPDATE user SET name = ?, password = ?, email= ? WHERE email = ?")
+        .bind(name)
+        .bind(password)
+        .bind(new_email)
+        .bind(old_email)
+        .execute(&pool)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if result.rows_affected() == 0 {
+        Ok(UpdateUserResult::UserNotFound)
+    } else {
+        Ok(UpdateUserResult::Updated)
+    }
+}
+
+pub async fn update_user_name(name: String, email: String) -> Result<UpdateUserResult, String> {
+    let pool = db::pool();
+
+    let result = sqlx::query("UPDATE user SET name = ? WHERE email = ?")
+        .bind(name)
+        .bind(email)
+        .execute(&pool)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if result.rows_affected() == 0 {
+        Ok(UpdateUserResult::UserNotFound)
+    } else {
+        Ok(UpdateUserResult::Updated)
+    }
+}
+
+pub async fn update_user_password(
+    password: String,
+    email: String,
+) -> Result<UpdateUserResult, String> {
+    let pool = db::pool();
+
+    let result = sqlx::query("UPDATE user SET password = ? WHERE email = ?")
+        .bind(password)
+        .bind(email)
+        .execute(&pool)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if result.rows_affected() == 0 {
+        Ok(UpdateUserResult::UserNotFound)
+    } else {
+        Ok(UpdateUserResult::Updated)
+    }
+}
+
+pub async fn delete_user(email: String) -> Result<DeleteUserResult, String> {
+    let pool = db::pool();
+    let result = sqlx::query("DELETE FROM user WHERE email = ?")
+        .bind(email)
+        .execute(&pool)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if result.rows_affected() == 0 {
+        Ok(DeleteUserResult::UserNotFound)
+    } else {
+        Ok(DeleteUserResult::Deleted)
     }
 }
