561a2d9804830e6451ff3343f78ff2af0a8fbb5c
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-12 12:07:09 -0300

    feat/adds system status update method

--- src-tauri/src/controllers/system_status_controller.rs
+++ src-tauri/src/controllers/system_status_controller.rs
@@ -1,6 +1,30 @@
-use crate::{models::SystemStatus, repository::system_status_repository};
+use crate::{dto::command_result::CommandResult, models::SystemStatus, repository::system_status_repository::{self, UpdateSystemStatusResult}};
 
 #[tauri::command]
 pub async fn get_system_status() -> Result<Vec<SystemStatus>, String> {
   system_status_repository::get_system_status().await
+}
+
+#[tauri::command]
+pub async fn update_system_status(system_status: Vec<SystemStatus>) -> Result<CommandResult, String> {
+    let mut list_fail = Vec::new();
+    for s_s in system_status {
+        let result =
+            system_status_repository::update_system_status(s_s.id_system_status, s_s.value).await?;
+        match result {
+            UpdateSystemStatusResult::SystemStatusUpdated => {}
+            UpdateSystemStatusResult::SystemStatusNotUpdated => {
+                list_fail.push(s_s.name);
+            }
+        }
+    }
+    Ok(CommandResult {
+        success: list_fail.is_empty(),
+        message_key: if list_fail.is_empty() {
+            "system_status.updated_sucessfully".into()
+        } else {
+            "system_status.failed_to_update".into()
+        },
+        list_fail,
+    })
 }
--- src-tauri/src/models/system_status.rs
+++ src-tauri/src/models/system_status.rs
@@ -1,4 +1,4 @@
-#[derive(sqlx::FromRow, serde::Serialize)]
+#[derive(sqlx::FromRow, serde::Serialize, serde::Deserialize)]
 pub struct SystemStatus {
     pub id_system_status: i64,
     pub name: String,
