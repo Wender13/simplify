f42f38d9392f59d7c1209da379131f8ee2133432
Wender <77487372+Wender13@users.noreply.github.com>
2026-08-15 10:31:31 -0300

    feat/adds load parameter method on splash screen for test, will be removed later

--- src/components/app/Splash.vue
+++ src/components/app/Splash.vue
@@ -1,12 +1,40 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { invoke } from "@tauri-apps/api/core";
+import { onBeforeMount } from "vue";
+
+interface Parameter {
+  id_parameter: number;
+  name: string;
+  value: string;
+}
+
+import { ref } from "vue";
+
+const parameters = ref<Parameter[]>([]);
+const language = ref<Parameter>({
+  id_parameter: 0,
+  name: "null",
+  value: "null",
+});
+onBeforeMount(async () => {
+  try {
+    parameters.value = await invoke<Parameter[]>("get_parameters");
+    language.value = parameters.value[0];
+    console.log("Parameters loaded: ", parameters.value);
+  } catch (error) {
+    console.error("Failed at getting parameters: ", error);
+  }
+});
+</script>
 
 <template>
   <div class="splash flex column center">
-    <img class="logo" src="/public/simplify.svg">
+    <img class="logo" src="/public/simplify.svg" />
     <div class="progress-container">
       <div class="progress-bar"></div>
-    </div> <br>
+    </div>
+    <br />
     <span class="dots text-container text-align-left">
       {{ "Carregando" }}
       <span>.</span><span>.</span><span>.</span>
@@ -26,7 +54,7 @@
 }
 
 .text-container {
-  width: clamp(60%,80%,400px);
-  height: clamp(2%,5%,15px);
+  width: clamp(60%, 80%, 400px);
+  height: clamp(2%, 5%, 15px);
 }
 </style>
